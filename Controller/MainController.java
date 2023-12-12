4356df8b3f0bc465b980107bc12fb5427f1487df
Francisco <franciscomarinalvarez@hotmail.es>
2023-12-13 00:08:32 +0100

    Finished without comments

--- Controller/MainController.java
+++ Controller/MainController.java
@@ -9,15 +9,23 @@ public class MainController {
     }
 
     public static void typeGameSelect(){
-        int option=MainMenu.mainMenu();
-        switch (option){
-            case 1:
-                WithoutBetController.withoutBetController();
-                break;
-            case 2:
-                BetController.betController();
-            default:
-                break;
-        }
+        int option=-1;
+        do {
+            option = MainMenu.mainMenu();
+            switch (option) {
+                case 1:
+                    WithoutBetController.withoutBetController();
+                    break;
+                case 2:
+                    BetController.betController();
+                    break;
+                case 3:
+                    System.out.println("Gracias por usar este programa!");
+                    break;
+                default:
+                    System.out.println("Opción incorrecta");
+                    break;
+            }
+        }while (option!=3);
     }
 }
